a60ea0585b6a6091faec89490feb54a0b1ff0d0e
JhonMercedes <jhonzera93@gmail.com>
2022-10-26 00:01:16 -0300

    finalizzando modal

--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -1,28 +1,70 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Box,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
 } from "@chakra-ui/react";
 import { useState } from "react";
 
-const ModalComp = ({data, setData, dataEdit, isOpen, onClose}) => {
-    const [name, setName] = useState(dataEdit.name || "");
-    const [email, setEmail] = useState(dataEdit.email || "");
-    return (
-        <>
-            <Modal isOpen={isOpen} onClose={onClose}>
-            </Modal>
-        </>
-    );
+const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
+  const [name, setName] = useState(dataEdit.name || "");
+  const [email, setEmail] = useState(dataEdit.email || "");
+
+  const handleSave = () => {
+    if(!name || !email) return;
+
+    if (emailAlreadyExists()) {
+        return alert("E-mail já cadastrado!")
+    }
+  }
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Cadastro Cliente</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl display="flex" flexDir="column" gap={4}>
+                <Box>
+                    <FormLabel>Nome</FormLabel>
+                    <Input 
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    
+                </Box><Box>
+                    <FormLabel>Nome</FormLabel>
+                    <Input 
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Box>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter justifyContent='start'>
+            <Button colorScheme='green' mr={3} onClick={handleSave}>
+                SALVAR
+            </Button>
+            <Button colorScheme="red" onClick={onClose}>
+                CANCELAR
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default ModalComp;
