565e480157687bcdbe001c58fd47a8ba3c3d1492
JhonMercedes <jhonzera93@gmail.com>
2022-10-26 16:17:05 -0300

    criando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
-const  App = () => {
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ModalComp from "./components/ModalComp";
 
-  return <h1>Hello</h1>;
+const  App = () => {
+  const { isOpen, onOpen, onClose} = useDisclosure();
+  const [data, setData] = useState([]);
+  const [dataEdit, setDataEdit] = useState({});
+
+  return (
+    <Flex 
+    h="100vh"
+    aling="center"
+    justify="center"
+    fontSize="20px"
+    fontFamily="poppins"
+    >
+      <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
+        <Button colorSheme="blue" onClick={() => [setDataEdit({}), onOpen({})]}>
+          Novo Cadastro
+        </Button>
+        <Box overFlow="auto" height="100%">
+        </Box>
+      </Box>
+    </Flex>
+  )
 };
 
 export default App;
